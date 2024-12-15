80e3073cfd88dad7a685e779e48e9cdbcc77e035
WhatsWrongOB <whatswrongobaid@gmail.com>
2024-12-15 15:45:09 +0000

    set cors configuration

--- server/src/routes/role.js
+++ server/src/routes/role.js
@@ -11,7 +11,7 @@ import { verifyAdminToken } from "../middlewares/index.js";
 const router = express.Router();
 
 router.post("/", verifyAdminToken, createRole);
-router.get("/", verifyAdminToken, getAllRoles);
+router.get("/", getAllRoles);
 router.get("/:roleID", verifyAdminToken, getRoleById);
 router.delete("/:roleID", verifyAdminToken, deleteRole);
 router.patch("/:roleID", verifyAdminToken, updateRole);
