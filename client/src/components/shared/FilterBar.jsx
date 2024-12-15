9b892d829f0223168297936949a6ac7d0bad84db
WhatsWrongOB <whatswrongobaid@gmail.com>
2024-12-15 12:49:07 +0000

    feature: filter for employees and export to excel

--- client/src/components/shared/FilterBar.jsx
+++ client/src/components/shared/FilterBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp, FaTimes } from "react-icons/fa";
 import { useSelector } from "react-redux";
 
 const FilterBar = ({ hideFilterBar, onApplyFilters }) => {
@@ -11,14 +11,20 @@ const FilterBar = ({ hideFilterBar, onApplyFilters }) => {
     roleName: "",
   });
 
+  const [toggleState, setToggleState] = useState({
+    department: true,
+    position: true,
+    status: true,
+  });
+
   const departments = useSelector((state) => state.department.departments);
   const role = useSelector((state) => state.role.roles);
   const statuses = ["Active", "Inactive", "Leave"];
 
   const handleToggle = (filterName) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [filterName]: prevFilters[filterName] ? null : prevFilters[filterName],
+    setToggleState((prevState) => ({
+      ...prevState,
+      [filterName]: !prevState[filterName],
     }));
   };
 
@@ -36,16 +42,18 @@ const FilterBar = ({ hideFilterBar, onApplyFilters }) => {
 
   return (
     <section className="fixed bg-gray-900 bg-opacity-20 inset-0 z-50">
-      <div
-        onClick={() => hideFilterBar(false)}
-        className="w-[20px] absolute top-3 right-[360px] h-[20px] text-black bg-gray-300 flex justify-center items-center rounded-full cursor-pointer"
-      >
-        <i className="fa-solid fa-xmark text-sm"></i>
-      </div>
       <aside
         id="overflow"
-        className="w-[70%] h-screen overflow-auto sm:w-[350px] rounded-md p-5 md:mb-0 bg-gray-800 font-bold fixed top-[70px] sm:top-0 right-0 z-50"
+        className="w-[75%] h-screen overflow-auto sm:w-[350px] rounded-md p-5 md:mb-0 bg-gray-800 font-bold fixed top-[70px] sm:top-0 right-0 z-50"
       >
+        <div className="w-full pb-4 mb-4 border-b border-gray-600">
+          <div
+            onClick={() => hideFilterBar(false)}
+            className="w-[20px] h-[20px] text-black bg-gray-300 flex justify-center items-center rounded-full cursor-pointer"
+          >
+            <FaTimes className="text-sm" />
+          </div>
+        </div>
         <div>
           {/* Department Filter */}
           <div className="h-auto border-b border-gray-500 pt-1">
@@ -54,33 +62,35 @@ const FilterBar = ({ hideFilterBar, onApplyFilters }) => {
               onClick={() => handleToggle("department")}
             >
               <h1 className="text-sm font-medium">Department</h1>
-              {filters.department ? (
+              {toggleState.department ? (
                 <FaChevronUp className="text-xs" />
               ) : (
                 <FaChevronDown className="text-xs" />
               )}
             </div>
-            <div>
-              {departments.map((department) => (
-                <div
-                  className="flex gap-2 items-center pb-5"
-                  key={department._id}
-                >
-                  <input
-                    type="checkbox"
-                    className="w-[10px] h-[9px] cursor-pointer"
-                    checked={filters.department === department._id}
-                    onChange={() => {
-                      handleCheckboxChange("department", department._id);
-                      handleCheckboxChange("departmentName", department.name);
-                    }}
-                  />
-                  <p className="text-[0.83rem] font-medium">
-                    {department.name}
-                  </p>
-                </div>
-              ))}
-            </div>
+            {toggleState.department && (
+              <div>
+                {departments.map((department) => (
+                  <div
+                    className="flex gap-2 items-center pb-5"
+                    key={department._id}
+                  >
+                    <input
+                      type="checkbox"
+                      className="w-[10px] h-[9px] cursor-pointer"
+                      checked={filters.department === department._id}
+                      onChange={() => {
+                        handleCheckboxChange("department", department._id);
+                        handleCheckboxChange("departmentName", department.name);
+                      }}
+                    />
+                    <p className="text-[0.83rem] font-medium">
+                      {department.name}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
 
           {/* Position Filter */}
@@ -90,28 +100,30 @@ const FilterBar = ({ hideFilterBar, onApplyFilters }) => {
               onClick={() => handleToggle("position")}
             >
               <h1 className="text-sm pb-2 font-medium">Position</h1>
-              {filters.position ? (
+              {toggleState.position ? (
                 <FaChevronUp className="text-xs" />
               ) : (
                 <FaChevronDown className="text-xs" />
               )}
             </div>
-            <div>
-              {role.map((role) => (
-                <div className="flex gap-2 items-center pb-5" key={role._id}>
-                  <input
-                    type="checkbox"
-                    className="w-[10px] h-[9px] cursor-pointer"
-                    checked={filters.role === role._id}
-                    onChange={() => {
-                      handleCheckboxChange("role", role._id);
-                      handleCheckboxChange("roleName", role.name);
-                    }}
-                  />
-                  <p className="text-[0.83rem] font-medium">{role.name}</p>
-                </div>
-              ))}
-            </div>
+            {toggleState.position && (
+              <div>
+                {role.map((role) => (
+                  <div className="flex gap-2 items-center pb-5" key={role._id}>
+                    <input
+                      type="checkbox"
+                      className="w-[10px] h-[9px] cursor-pointer"
+                      checked={filters.role === role._id}
+                      onChange={() => {
+                        handleCheckboxChange("role", role._id);
+                        handleCheckboxChange("roleName", role.name);
+                      }}
+                    />
+                    <p className="text-[0.83rem] font-medium">{role.name}</p>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
 
           {/* Status Filter */}
@@ -121,34 +133,35 @@ const FilterBar = ({ hideFilterBar, onApplyFilters }) => {
               onClick={() => handleToggle("status")}
             >
               <h1 className="text-sm font-medium">Status</h1>
-              {filters.status ? (
+              {toggleState.status ? (
                 <FaChevronUp className="text-xs" />
               ) : (
                 <FaChevronDown className="text-xs" />
               )}
             </div>
-            <div>
-              {statuses.map((status) => (
-                <div className="flex gap-2 items-center pb-5" key={status}>
-                  <input
-                    type="checkbox"
-                    className="w-[10px] h-[9px] cursor-pointer"
-                    checked={filters.status === status}
-                    onChange={() => handleCheckboxChange("status", status)}
-                  />
-                  <p className="text-[0.83rem] font-medium">{status}</p>
-                </div>
-              ))}
-            </div>
+            {toggleState.status && (
+              <div>
+                {statuses.map((status) => (
+                  <div className="flex gap-2 items-center pb-5" key={status}>
+                    <input
+                      type="checkbox"
+                      className="w-[10px] h-[9px] cursor-pointer"
+                      checked={filters.status === status}
+                      onChange={() => handleCheckboxChange("status", status)}
+                    />
+                    <p className="text-[0.83rem] font-medium">{status}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+            <button
+              className="w-full mb-[70px] sm:mb-0 text-sm sm:text-base border border-gray-600 bg-gradient-to-r from-blue-700 via-blue-600 to-blue-800 hover:bg-gradient-to-r hover:from-blue-800 hover:via-blue-700 hover:to-blue-600 p-3 rounded-lg mt-3"
+              onClick={handleApplyFilters}
+            >
+              Apply Filters
+            </button>
           </div>
         </div>
-
-        <button
-          className="w-full border border-gray-600 bg-gradient-to-r from-blue-700 via-blue-600 to-blue-800 hover:bg-gradient-to-r hover:from-blue-800 hover:via-blue-700 hover:to-blue-600  p-3 rounded-lg mt-3"
-          onClick={handleApplyFilters}
-        >
-          Apply Filters
-        </button>
       </aside>
     </section>
   );
