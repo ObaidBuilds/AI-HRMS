98bb7d553c179c5ce28357c7841467c989e739aa
WhatsWrongOB <whatswrongobaid@gmail.com>
2024-12-29 11:32:15 +0000

    enhance ui & set responsiveness

--- client/src/admin/feedback/Feedback.jsx
+++ client/src/admin/feedback/Feedback.jsx
@@ -7,7 +7,6 @@ import Loader from "../../components/shared/loaders/Loader";
 import Pagination from "../../components/shared/others/Pagination";
 
 function Feedback() {
-
   const dispatch = useDispatch();
   const { feedbacks, loading, pagination } = useSelector(
     (state) => state.feedback
@@ -31,96 +30,99 @@ function Feedback() {
     <>
       {loading && <Loader />}
 
-        <section className="bg-secondary min-h-auto p-3 sm:p-4 rounded-lg">
-          <div className="mb-4 sm:px-4 flex flex-wrap items-center gap-2 sm:gap-3">
-            {filters.map((filter, i) => (
-              <button
-                key={i}
-                onClick={() => setReviewFilter(filter.value)}
-                className={`flex flex-grow sm:flex-grow-0 justify-center items-center gap-2 text-[0.81rem] sm:text-[0.9rem]  border py-1 px-5 rounded-3xl font-semibold ${
-                  reviewFilter === filter.value
-                    ? "border-blue-500 ring-1 ring-blue-500"
-                    : "border-gray-300"
-                } focus:outline-none focus:ring-1 focus:ring-blue-500`}
-              >
-                <i className={`text-xs fas ${filter.icon}`}></i>
-                {filter.label}
-              </button>
-            ))}
-          </div>
+      <section className="bg-secondary min-h-auto p-3 sm:p-4 rounded-lg">
+        <div className="mb-4 sm:px-4 flex flex-wrap items-center gap-2 sm:gap-3">
+          {filters.map((filter, i) => (
+            <button
+              key={i}
+              onClick={() => setReviewFilter(filter.value)}
+              className={`flex flex-grow sm:flex-grow-0 justify-center items-center gap-2 text-[0.81rem] sm:text-[0.9rem]  border py-1 px-5 rounded-3xl font-semibold ${
+                reviewFilter === filter.value
+                  ? "border-blue-500 ring-1 ring-blue-500"
+                  : "border-gray-300"
+              } focus:outline-none focus:ring-1 focus:ring-blue-500`}
+            >
+              <i className={`text-xs fas ${filter.icon}`}></i>
+              {filter.label}
+            </button>
+          ))}
+        </div>
 
-          <div id="overflow" className="overflow-x-auto max-h-auto min-h-[70vh]">
-            <table className="min-w-full text-left table-auto border-collapse text-sm whitespace-nowrap">
-              <thead>
-                <tr className="bg-gray-600 text-gray-200">
-                  {[
-                    "Emp ID",
-                    "Name",
-                    "Department",
-                    "Position",
-                    "AI Review",
-                    "Description",
-                    "Date",
-                    "Rating",
-                  ].map((header, index) => (
-                    <th
-                      key={index}
-                      className="py-3 px-4 border-b border-gray-500"
-                    >
-                      {header}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {feedbacks.map((feedback, index) => (
-                  <tr
+        <div
+          id="overflow"
+          className="overflow-x-auto max-h-auto min-h-[68vh] sm:min-h-[75vh]"
+        >
+          <table className="min-w-full text-left table-auto border-collapse text-sm whitespace-nowrap">
+            <thead>
+              <tr className="bg-gray-600 text-gray-200">
+                {[
+                  "Emp ID",
+                  "Name",
+                  "Department",
+                  "Position",
+                  "AI Review",
+                  "Description",
+                  "Date",
+                  "Rating",
+                ].map((header, index) => (
+                  <th
                     key={index}
-                    className="even:bg-gray-800 odd:bg-gray-700 hover:bg-gray-600"
+                    className="py-3 px-4 border-b border-gray-500"
                   >
-                    <td className="py-3 px-4 border-b border-gray-500">
-                      {feedback.employee.employeeId}
-                    </td>
-                    <td className="py-3 px-4 border-b border-gray-500">
-                      {feedback.employee.name}
-                    </td>
-                    <td className="py-3 px-4 border-b border-gray-500">
-                      {feedback.employee.department.name}
-                    </td>
-                    <td className="py-3 px-4 border-b border-gray-500">
-                      {feedback.employee.role.name}
-                    </td>
-                    <td className="py-3 px-4 border-b border-gray-500">
-                      {feedback.review}
-                    </td>
-                    <td className="py-3 px-4 border-b border-gray-500">
-                      {feedback.description.slice(0, 10) + "..."}
-                    </td>
-                    <td className="py-3 px-4 border-b border-gray-500">
-                      {formatDate(feedback.createdAt)}
-                    </td>
-                    <td className="py-3 px-4 border-b border-gray-500 flex items-center gap-2">
-                      {feedback.rating} <FaStar color="gold" />
-                    </td>
-                  </tr>
+                    {header}
+                  </th>
                 ))}
-              </tbody>
-            </table>
+              </tr>
+            </thead>
+            <tbody>
+              {feedbacks.map((feedback, index) => (
+                <tr
+                  key={index}
+                  className="even:bg-gray-800 odd:bg-gray-700 hover:bg-gray-600"
+                >
+                  <td className="py-3 px-4 border-b border-gray-500">
+                    {feedback.employee.employeeId}
+                  </td>
+                  <td className="py-3 px-4 border-b border-gray-500">
+                    {feedback.employee.name}
+                  </td>
+                  <td className="py-3 px-4 border-b border-gray-500">
+                    {feedback.employee.department.name}
+                  </td>
+                  <td className="py-3 px-4 border-b border-gray-500">
+                    {feedback.employee.role.name}
+                  </td>
+                  <td className="py-3 px-4 border-b border-gray-500">
+                    {feedback.review}
+                  </td>
+                  <td className="py-3 px-4 border-b border-gray-500">
+                    {feedback.description.slice(0, 10) + "..."}
+                  </td>
+                  <td className="py-3 px-4 border-b border-gray-500">
+                    {formatDate(feedback.createdAt)}
+                  </td>
+                  <td className="py-3 px-4 border-b border-gray-500 flex items-center gap-2">
+                    {feedback.rating} <FaStar color="gold" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-            {!loading && feedbacks.length === 0 && (
-              <div className="w-full h-[50vh] flex flex-col justify-center items-center">
-                <i className="fas fa-ban text-2xl text-gray-400"></i>
-                <p className="mt-2 text-base text-gray-400">No feedback found</p>
-              </div>
-            )}
-          </div>
+          {!loading && feedbacks.length === 0 && (
+            <div className="w-full h-[50vh] flex flex-col justify-center items-center">
+              <i className="fas fa-ban text-2xl text-gray-400"></i>
+              <p className="mt-2 text-base text-gray-400">No feedback found</p>
+            </div>
+          )}
+        </div>
 
-          <Pagination
-            currentPage={currentPage}
-            totalPages={pagination?.totalPages}
-            onPageChange={setCurrentPage}
-          />
-        </section>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={pagination?.totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </section>
     </>
   );
 }
