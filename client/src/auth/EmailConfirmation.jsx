4e1d52624f46911ff6cf91b6f2592fa9d42b829a
WhatsWrongOB <whatswrongobaid@gmail.com>
2025-03-15 10:30:28 +0000

    enhance the design of email confirmation page

--- client/src/auth/EmailConfirmation.jsx
+++ client/src/auth/EmailConfirmation.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const EmailConfirmation = () => {
   return (
-    <div className="h-[85vh] flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
+    <div className="h-[85vh] flex justify-center items-center"> 
+      <div className="flex flex-col justify-center items-center p-8 rounded-lg"> 
         <img
-          className="w-[350px] sm:w-[430px] h-[250px]"
+          className="w-[280px] sm:w-[430px] h-[250px]"
           src="/verify.avif"
           alt="verify_email"
         />
-        <p className="text-[0.93rem] font-medium">
-          Please check you email to reset your password
-        </p>
+        <div className="flex items-center mt-4"> 
+          <p className="font-bold text-gray-800 text-center"> 
+            Check your email to reset your password
+          </p>
+        </div>
       </div>
     </div>
   );
