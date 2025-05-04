115665ae7eec8dc07d350f329fce4cf09f54a5cc
WhatsWrongOB <whatswrongobaid@gmail.com>
2025-05-04 14:24:33 +0000

    -add lodaer txt file to proof property

--- client/src/components/shared/cards/InfoCard.jsx
+++ client/src/components/shared/cards/InfoCard.jsx
@@ -12,7 +12,7 @@ const InfoCard = ({ detail }) => {
   return (
     <div className="w-full flex-grow relative md:w-[32.8%] h-[158px] rounded-lg dark:text-gray-200 text-gray-700 bg-gray-100 dark:bg-secondary border border-gray-300 dark:border-primary p-4 shadow flex flex-col">
       <div className="flex justify-between items-start">
-        <h3 className="font-semibold text-base sm:text-[0.95rem] mb-3">
+        <h3 className="font-semibold text-base mb-3">
           {detail.title}
         </h3>
         {/* <button
