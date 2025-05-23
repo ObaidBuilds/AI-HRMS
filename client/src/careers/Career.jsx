557cc56e8f95e0b7f8a51453e5bd4649b4929f05
WhatsWrongOB <whatswrongobaid@gmail.com>
2025-05-23 11:17:33 +0000

    -add recruitment management module

--- client/src/careers/Career.jsx
+++ client/src/careers/Career.jsx
@@ -97,7 +97,7 @@ const Career = () => {
 
       {/* Jobs Section */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-16">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           {jobs.map((job) => (
             <div
               key={job._id}
