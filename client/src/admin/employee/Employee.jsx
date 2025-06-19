594707a47e2a5cdd76bdd869987a9b4fa606261d
WhatsWrongOB <whatswrongobaid@gmail.com>
2025-06-19 11:04:48 +0000

    -fix the reponsivenes of taels

--- client/src/admin/employee/Employee.jsx
+++ client/src/admin/employee/Employee.jsx
@@ -177,7 +177,7 @@ function Employee() {
 
         <div
           id="overflow"
-          className="overflow-x-auto min-h-[90vh] sm:min-h-[80vh]"
+          className="overflow-x-auto sm:min-h-[80vh]"
         >
           <table className="min-w-full text-left table-auto border-collapse text-[0.83rem] whitespace-nowrap">
             <thead>
