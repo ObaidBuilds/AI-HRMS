9e92510ac99330affeda7bc275e3a665d5aa7847
WhatsWrongOB <whatswrongobaid@gmail.com>
2025-08-17 15:17:08 +0000

    -set height of the loader component

--- client/src/components/shared/loaders/ComponentLoader.jsx
+++ client/src/components/shared/loaders/ComponentLoader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ComponentLoader = () => {
   return (
-    <div className="h-[85vh] md:h-[95vh] flex justify-center items-center">
+    <div className="h-[89vh] md:h-[97vh] flex justify-center items-center">
       <div className="loading-container">
         <div className="loading-circle"></div>
         <div className="loading-circle"></div>
